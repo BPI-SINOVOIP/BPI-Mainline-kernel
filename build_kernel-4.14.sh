69f76f2f966a68d19c4405f5cd1e251426f64a74
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2018-10-23 17:25:13 +0800

    support build 4.14 / 4.19

--- build_kernel-4.14.sh
+++ build_kernel-4.14.sh
@@ -2,5 +2,5 @@
 TOPDIR=`pwd`
 TOOLCHAIN=gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf
 export PATH=$TOPDIR/toolchains/$TOOLCHAIN/bin:$PATH
-cd linux-4.19
+cd linux-4.14
 ./build.sh
--- build_kernel_64-4.14.sh
+++ build_kernel_64-4.14.sh
@@ -2,5 +2,5 @@
 TOPDIR=`pwd`
 TOOLCHAIN=gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu
 export PATH=$TOPDIR/toolchains/$TOOLCHAIN/bin:$PATH
-cd linux-4.19
+cd linux-4.14
 ./build_64.sh
