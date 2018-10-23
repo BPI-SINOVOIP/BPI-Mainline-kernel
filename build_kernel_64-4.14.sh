4a3e46feb919aefd76da9bcbbe48eba419afe2aa
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2018-10-23 19:31:55 +0800

    toolchains: update gcc-linaro-7.3.1-2018.05

--- build_kernel_64-4.14.sh
+++ build_kernel_64-4.14.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TOPDIR=`pwd`
-TOOLCHAIN=gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu
+TOOLCHAIN=gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu
 export PATH=$TOPDIR/toolchains/$TOOLCHAIN/bin:$PATH
 cd linux-4.14
 ./build_64.sh
