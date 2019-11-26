4fc2d57694b8ba0538b87c03d0300473571f5fce
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2019-11-26 20:04:44 +0800

    linux-5.4: add nproc

--- linux-5.4/build.sh
+++ linux-5.4/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+nproc=$(cat /proc/cpuinfo | grep processor | wc -l )
+echo nproc=$nproc
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 export KBUILD_OUTPUT=output/bpi-all
 mkdir -p $KBUILD_OUTPUT
 make bpi_defconfig
-make -j8 dtbs
-LOADADDR=0x40008000 make -j8 uImage
-make -j8 modules
-make -j8 INSTALL_MOD_PATH=out modules_install
+make -j$nproc dtbs
+LOADADDR=0x40008000 make -j$nproc uImage
+make -j$nproc modules
+make -j$nproc INSTALL_MOD_PATH=out modules_install
 
+#make headers_install
